b8d70facd29cea97de52dd59eb8671b968b15825
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-07-13 00:16:15 +0900

    [feature/#18] 카테고리에 맞는 대처법 추가

--- src/models/Review.ts
+++ src/models/Review.ts
@@ -2,6 +2,26 @@ import mongoose from "mongoose";
 import Review from "../interface/Review";
 import { MongoEntity } from "./Model";
 
-const ReviewSchema = new mongoose.Schema({});
+const ReviewSchema = new mongoose.Schema({
+  restaurantId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: "Restaurant",
+  },
+  writerId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+  content: {
+    type: String,
+  },
+  image: [{ type: String }],
+  reason: [{ type: String }],
+});
 
 export default mongoose.model<MongoEntity<Review>>("Review", ReviewSchema);
