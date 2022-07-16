39bbc870b85a5113688f5c4157205716288b3a3b
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-07-16 23:03:55 +0900

    [feature/#87] ReviewRouter 제작

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@
 import { Router } from "express";
 import AuthRouter from "./AuthRouter";
 import RestaurantRouter from "./RestaurantRouter";
+import ReviewRouter from "./ReviewRouter";
 import UserRouter from "./UserRouter";
 
 const router: Router = Router();
@@ -9,5 +10,6 @@ const router: Router = Router();
 router.use("/auth", AuthRouter);
 router.use("/restaurant", RestaurantRouter);
 router.use("/user", UserRouter);
+router.use("/review", ReviewRouter);
 
 export default router;
