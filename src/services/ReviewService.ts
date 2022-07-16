0f90bdd3863057042d479327393d6dda3e7d4572
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-07-16 22:53:15 +0900

    [feature/#87] ReviewService에서 DTO 적용

--- src/services/ReviewService.ts
+++ src/services/ReviewService.ts
@@ -1,10 +1,27 @@
+import GetReviewsDto from "../controllers/dto/review/GetReviewsDto";
 import IUser from "../interface/User";
 import Review from "../models/Review";
 
 const getReviewsByRestaurant = async (id: string) => {
-  const reivews = await Review.find({
+  const reviews = await Review.find({
     restaurantId: id,
-  }).populate<{ writer: IUser }>("writerId");
+  }).populate<{ writerId: IUser }>("writerId");
 
-  return reivews;
+  const reviewDto: GetReviewsDto[] = reviews.map((review) => {
+    return {
+      writer: review.writerId,
+      score: review.score,
+      content: review.content,
+      image: review.image,
+      reason: {
+        taste: review.reason.taste,
+        good: review.reason.good,
+      },
+    };
+  });
+  return reviewDto;
+};
+
+export default {
+  getReviewsByRestaurant,
 };
