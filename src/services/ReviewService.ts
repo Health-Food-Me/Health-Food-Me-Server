8b8277eda1ba4c6c4142bffe1105326417fe977c
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-07-17 00:17:35 +0900

    [feature/#95] ReviewService 유저당 리뷰 구현

--- src/services/ReviewService.ts
+++ src/services/ReviewService.ts
@@ -22,6 +22,27 @@ const getReviewsByRestaurant = async (id: string) => {
   return reviewDto;
 };
 
+const getReviewsByUser = async (id: string) => {
+  const reviews = await Review.find({
+    writer: id,
+  }).populate<{ writer: IUser }>("writer");
+
+  const reviewDto: GetReviewsDto[] = reviews.map((review) => {
+    return {
+      writer: review.writer,
+      score: review.score,
+      content: review.content,
+      image: review.image,
+      reason: {
+        taste: review.hashtag.taste,
+        good: review.hashtag.good,
+      },
+    };
+  });
+  return reviewDto;
+};
+
 export default {
   getReviewsByRestaurant,
+  getReviewsByUser,
 };
