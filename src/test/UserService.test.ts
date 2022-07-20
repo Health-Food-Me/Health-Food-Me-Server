1389c6e07fadb43b1fd5dc955449ea5a1372695d
thguss <thgus345@naver.com>
2022-07-21 01:02:14 +0900

    [feature/#test]  UserService 테스트 코드

--- src/test/UserService.test.ts
+++ src/test/UserService.test.ts
@@ -6,6 +6,21 @@ describe("유저 프로필 조회 테스트", () => {
     configMongoose();
     const user = await UserService.getUserProfile("62d1c081c4beaf1e397b5d40");
     expect(user?._id).toBe("62d1c081c4beaf1e397b5d40");
-    expect(user?.name).toBe("헬푸미2342999768");
+    expect(user?.name).toBe("로리롤리");
+  });
+});
+
+describe("유저 프로필 수정 테스트", () => {
+  test("PUT /user/:userId/profile", async () => {
+    configMongoose();
+    const user = await UserService.updateUserProfile(
+      "62d1c081c4beaf1e397b5d40",
+      "크리넥스",
+    );
+    const result = {
+      _id: "62d1c081c4beaf1e397b5d40",
+      name: "크리넥스",
+    };
+    expect(user).toStrictEqual(result);
   });
 });
