79b671dfda7105e90ddf8b84e00081a98f9fd9cf
thguss <thgus345@naver.com>
2022-08-31 15:47:56 +0900

    [DELETE] 둘러보기 기능 미들웨어 삭제

--- src/routes/RestaurantRouter.ts
+++ src/routes/RestaurantRouter.ts
@@ -1,33 +1,13 @@
 import { Router } from "express";
 import RestaurantController from "../controllers/RestaurantController";
-import auth from "../middleware/auth";
 
 const router = Router();
 
-router.get(
-  "/search/auto",
-  auth,
-  RestaurantController.getSearchAutoCompleteResult,
-);
-
-router.get("/search/card", auth, RestaurantController.searchRestaurantCardList);
-router.get(
-  "/:restaurantId/:userId/menus",
-  auth,
-  RestaurantController.getMenuDetail,
-);
-
-router.get("/", auth, RestaurantController.getAroundRestaurants);
-router.get(
-  "/:restaurantId/prescription",
-  auth,
-  RestaurantController.getPrescription,
-);
-
-router.get(
-  "/:restaurantId/:userId",
-  auth,
-  RestaurantController.getRestaurantSummary,
-);
+router.get("/search/auto", RestaurantController.getSearchAutoCompleteResult);
+router.get("/search/card", RestaurantController.searchRestaurantCardList);
+router.get("/:restaurantId/:userId/menus", RestaurantController.getMenuDetail);
+router.get("/", RestaurantController.getAroundRestaurants);
+router.get("/:restaurantId/prescription", RestaurantController.getPrescription);
+router.get("/:restaurantId/:userId", RestaurantController.getRestaurantSummary);
 
 export default router;
