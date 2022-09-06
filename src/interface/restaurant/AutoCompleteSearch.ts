ba1a404f22ceb9e5fe9474875deb8acf38925f7b
thguss <thgus345@naver.com>
2022-09-06 20:03:27 +0900

    [feature/#290] ADD : 사용자 위치 (경도&위도) 추가

--- src/interface/restaurant/AutoCompleteSearch.ts
+++ src/interface/restaurant/AutoCompleteSearch.ts
@@ -5,6 +5,9 @@ interface AutoCompleteSearch {
   name: string;
   isDietRestaurant: boolean;
   isCategory: boolean;
+  distance: number;
+  longitude: number;
+  latitude: number;
 }
 
 export default AutoCompleteSearch;
