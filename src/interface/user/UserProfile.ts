7f2aaab86eccd48bdf64346acd02dabca0d53db9
thguss <thgus345@naver.com>
2022-09-11 19:29:00 +0900

    [feature/#306] FIX: 사용하지 않는 데이터 필드 삭제

--- src/interface/user/UserProfile.ts
+++ src/interface/user/UserProfile.ts
@@ -1,9 +1,6 @@
-import { Types } from "mongoose";
-
 interface UserProfiile {
   _id: string;
   name: string;
-  scrapRestaurants: (string | Types.ObjectId)[] | undefined;
 }
 
 export default UserProfiile;
